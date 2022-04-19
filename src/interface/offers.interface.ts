d964cd66c1453180a0e4b2f9ebb3659398a99eed
karpss <ola.fam10@gmail.com>
2022-04-19 20:00:03 +0100

    deleted unused import from offers model interface

--- src/interface/offers.interface.ts
+++ src/interface/offers.interface.ts
@@ -1,15 +1,11 @@
 /* eslint-disable */
-
-import { ByteLengthQueuingStrategy } from "stream/web"
-
 export interface OffersModel {
-"id": string,
-"description": string,
-"splashImages": any,
-"prices": any
-
+  id: string;
+  description: string;
+  splashImages: any;
+  prices: any;
 }
 
 export interface OffersArrayModel {
-    all_offers:OffersModel[]
+  all_offers: OffersModel[];
 }
